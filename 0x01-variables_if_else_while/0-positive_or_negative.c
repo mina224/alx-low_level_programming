9b14bca5e846114c51758180d37cc88c05f0d4ee
mina224 <salimmina027@gmail.com>
2023-03-19 17:57:54 -0700

    git commit -m 'posneg' git push betty 0-positive_or_negative.c~~ < q

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
 /**
- * main - Entry point
+ * main - main function
  * Return: Always 0
  */
 int main(void)
@@ -11,19 +13,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+	/* your code goes there */
 	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
+		printf("%d is positive", n);
 	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
+		printf("%d is zero", n);
 	else
-	{
-		printf("%d is negative\n", n);
-	}
+	       	printf("%d is negative", n);
+	printf("\n");
 	return (0);
 }
 
