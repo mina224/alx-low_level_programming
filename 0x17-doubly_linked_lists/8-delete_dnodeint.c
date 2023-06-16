22d0ab81de7272afcc8aa7dcf64329382781dd9d
mina224 <salimmina027@gmail.com>
2023-06-15 17:23:20 -0700

    doubly_linked_lists C

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,7 +10,6 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
     dlistint_t *current = *head;
-    unsigned int i;
 
     if (*head == NULL)
         return -1;
@@ -24,7 +23,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
         return 1;
     }
 
-    for (i = 0; current != NULL && i < index; i++)
+    for (unsigned int i = 0; current != NULL && i < index; i++)
     {
         current = current->next;
     }
